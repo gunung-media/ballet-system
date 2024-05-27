5a189b05c5bb414ed7a276fcb2c5b1c33a604043
Richie-Z <zakariarichie@yahoo.com>
2024-05-27 20:52:40 +0700

    chore: updated hero icon and sidebar

--- app/Enums/HeroIconEnum.php
+++ app/Enums/HeroIconEnum.php
@@ -5,6 +5,12 @@ namespace App\Enums;
 enum HeroIconEnum
 {
     case home;
+    case tag;
+    case wrenchScrewdriver;
+    case sale;
+    case users;
+    case book;
+    case logout;
 
     public function toSvg(): string
     {
@@ -16,6 +22,64 @@ enum HeroIconEnum
                       height="15px"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                       <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+                html;
+            case self::tag:
+                return <<< 'html'
+                    <svg
+                      width="15px"
+                      height="15px"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
+                    </svg>
+                html;
+
+            case self::wrenchScrewdriver:
+                return <<< 'html'
+                    <svg
+                      width="15px"
+                      height="15px"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+                    </svg>
+                html;
+
+            case self::sale:
+                return <<< 'html'
+                    <svg
+                      width="15px"
+                      height="15px"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                html;
+
+            case self::users:
+                return <<< 'html'
+                    <svg
+                      width="15px"
+                      height="15px"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                    </svg>
+                html;
+            case self::book:
+                return <<< 'html'
+                    <svg
+                      width="15px"
+                      height="15px"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                    </svg>
+                html;
+            case self::logout:
+                return <<< 'html'
+                    <svg
+                      width="15px"
+                      height="15px"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
                 html;
         }
--- app/View/Components/Sidebar.php
+++ app/View/Components/Sidebar.php
@@ -21,9 +21,22 @@ class Sidebar extends Component
         $this->items = [
             new SidebarItem(title: 'Beranda', url: route('dashboard'), icon: HeroIconEnum::home),
             "Penjualan",
-            new SidebarItem(title: 'Kategori', url: route('kategori.index'), icon: HeroIconEnum::home),
+            new SidebarItem(title: 'Kategori', url: route('kategori.index'), icon: HeroIconEnum::tag),
+            new SidebarItem(title: 'Data Barang', url: route('kategori.index'), icon: HeroIconEnum::wrenchScrewdriver),
+            new SidebarItem(title: 'Penjualan', url: route('kategori.index'), icon: HeroIconEnum::sale),
+            "Penyewaan",
+            new SidebarItem(title: 'Inventori', url: route('dashboard'), icon: HeroIconEnum::home),
+            new SidebarItem(title: 'Transaksi Penyewaan', url: route('dashboard'), icon: HeroIconEnum::sale),
             "Kursus",
-            new SidebarItem(title: 'Data Siswa', url: route('siswa.index'), icon: HeroIconEnum::home),
+            new SidebarItem(title: 'Data Siswa', url: route('siswa.index'), icon: HeroIconEnum::users),
+            new SidebarItem(title: 'Data Guru', url: route('siswa.index'), icon: HeroIconEnum::users),
+            new SidebarItem(title: 'Data Kelas', url: route('siswa.index'), icon: HeroIconEnum::home),
+            new SidebarItem(title: 'Data Absensi', url: route('siswa.index'), icon: HeroIconEnum::home),
+            new SidebarItem(title: 'Pembayaran SPP', url: route('siswa.index'), icon: HeroIconEnum::home),
+            "Lain-Lain",
+            new SidebarItem(title: 'Data Admin', url: route('sign-in'), icon: HeroIconEnum::users),
+            new SidebarItem(title: 'Panduan', url: route('sign-in'), icon: HeroIconEnum::book),
+            new SidebarItem(title: 'Logout', url: route('sign-in'), icon: HeroIconEnum::logout),
         ];
     }
 

5d086d5e19f072ebbdf0d615c02abd92f6a5fe9d
Richie-Z <zakariarichie@yahoo.com>
2024-05-31 20:25:55 +0700

    feat(controller): introduce user auth controller

---
+++ app/Http/Controllers/Authentication/UserAuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Authentication;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\UserRepository;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserAuthController extends Controller
+{
+    public function __construct(
+        protected UserRepository $userRepository,
+    ) {
+    }
+
+    public function showLoginForm(): View|Factory
+    {
+        return view('sign-in');
+    }
+
+    public function login(Request $request): RedirectResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+        return redirect()->back()->withErrors(['email' => 'Invalid email or password.'])->withInput();
+    }
+
+    public function logout(): Redirector|RedirectResponse
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+}

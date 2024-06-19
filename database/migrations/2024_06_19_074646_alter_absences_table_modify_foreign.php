3a5164733f6636959f901b883cf21ef8ad53a3da
Richie-Z <zakariarichie@yahoo.com>
2024-06-19 14:47:42 +0700

    fix: absence foreign key fail if edit class

---
+++ database/migrations/2024_06_19_074646_alter_absences_table_modify_foreign.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('absences', function (Blueprint $table) {
+            $table->dropForeign(['class_schedule_id']);
+            $table->foreign('class_schedule_id')
+                ->references('id')
+                ->on('class_schedules')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('absences', function (Blueprint $table) {
+            $table->dropForeign(['class_schedule_id']);
+            $table->foreign('class_schedule_id')
+                ->references('id')
+                ->on('class_schedules');
+        });
+    }
+};

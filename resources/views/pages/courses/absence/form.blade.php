4a6ae98ec27927ad7365848402d5fb2b980b2578
Richie-Z <zakariarichie@yahoo.com>
2024-06-21 16:21:49 +0700

    pref: disable absence btn if is past

--- resources/views/pages/courses/absence/form.blade.php
+++ resources/views/pages/courses/absence/form.blade.php
@@ -13,7 +13,8 @@
                 <div class="card-header p-0 mx-3 mt-3 position-relative z-index-1">
                     @php
                         use Carbon\Carbon;
-                        $formattedDate = Carbon::parse($date)->translatedFormat('l, d F Y, H:i');
+                        $date = Carbon::parse($date);
+                        $formattedDate = $date->translatedFormat('l, d F Y, H:i');
                     @endphp
                     <h5>{{ $class->name }}</h5>
                     <p class="font-weight-light text-sm">Tanggal: {{ $formattedDate }}</p>
@@ -64,7 +65,8 @@
                                                     <button type="submit" name="state" value="Hadir"
                                                         class="btn btn-success w-100 btn-tooltip" data-bs-toggle="tooltip"
                                                         data-bs-placement="top" title="Saya Hadir" data-container="body"
-                                                        data-animation="true" {{ is_null($absence) ? 'disabled' : '' }}>
+                                                        data-animation="true"
+                                                        {{ is_null($absence) || $date->isPast() ? 'disabled' : '' }}>
                                                         <span class="btn-inner--icon"><i class="ni ni-active-40"></i></span>
                                                         <span class="btn-inner--text">Hadir</span>
                                                     </button>
@@ -73,7 +75,8 @@
                                                     <button type="submit" name="state" value="Ijin"
                                                         class="btn btn-warning w-100 btn-tooltip" data-bs-toggle="tooltip"
                                                         data-bs-placement="top" title="Saya Ijin" data-container="body"
-                                                        data-animation="true" {{ is_null($absence) ? 'disabled' : '' }}>
+                                                        data-animation="true"
+                                                        {{ is_null($absence) || $date->isPast() ? 'disabled' : '' }}>
                                                         <span class="btn-inner--icon"><i class="ni ni-planet"></i></span>
                                                         <span class="btn-inner--text">Ijin</span>
                                                     </button>
@@ -82,7 +85,8 @@
                                                     <button type="submit" name="state" value="Sakit"
                                                         class="btn btn-primary w-100 btn-tooltip" data-bs-toggle="tooltip"
                                                         data-bs-placement="top" title="Saya Sakit" data-container="body"
-                                                        data-animation="true" {{ is_null($absence) ? 'disabled' : '' }}>
+                                                        data-animation="true"
+                                                        {{ is_null($absence) || $date->isPast() ? 'disabled' : '' }}>
                                                         <span class="btn-inner--icon"><i class="ni ni-diamond"></i></span>
                                                         <span class="btn-inner--text">Sakit</span>
                                                     </button>
@@ -92,7 +96,7 @@
                                                         class="btn btn-danger w-100 btn-tooltip" data-bs-toggle="tooltip"
                                                         data-bs-placement="top" title="Saya Tidak Hadir"
                                                         data-container="body" data-animation="true"
-                                                        {{ is_null($absence) ? 'disabled' : '' }}>
+                                                        {{ is_null($absence) || $date->isPast() ? 'disabled' : '' }}>
                                                         <span class="btn-inner--icon"><i class="ni ni-user-run"></i></span>
                                                         <span class="btn-inner--text">Tidak Hadir</span>
                                                     </button>

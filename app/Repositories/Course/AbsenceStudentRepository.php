37e8ca549b6cefc2c4fe43b614953869f109e8c0
Richie-Z <zakariarichie@yahoo.com>
2024-08-27 20:47:26 +0700

    feat: message when already absence

--- app/Repositories/Course/AbsenceStudentRepository.php
+++ app/Repositories/Course/AbsenceStudentRepository.php
@@ -8,8 +8,7 @@ class AbsenceStudentRepository
 {
     public function __construct(
         protected AbsenceStudent $model
-    ) {
-    }
+    ) {}
 
     public function createOrUpdate(array $data): AbsenceStudent
     {
@@ -17,6 +16,12 @@ class AbsenceStudentRepository
             'absence_id' => $data['absence_id'],
             'student_id' => $data['student_id']
         ];
+
+        $existingRecord = $this->model->where($attributes)->first();
+
+        if ($existingRecord) {
+            throw new \Exception('Student has already been marked absent.');
+        }
 
         return $this->model->updateOrCreate($attributes, $data);
     }

30e8ce9c79e09a509f80795a036d5b706504d6a2
Richie-Z <zakariarichie@yahoo.com>
2024-08-28 17:50:39 +0700

    Revert "feat: message when already absence"
    This reverts commit 37e8ca549b6cefc2c4fe43b614953869f109e8c0.

--- app/Repositories/Course/AbsenceStudentRepository.php
+++ app/Repositories/Course/AbsenceStudentRepository.php
@@ -8,7 +8,8 @@ class AbsenceStudentRepository
 {
     public function __construct(
         protected AbsenceStudent $model
-    ) {}
+    ) {
+    }
 
     public function createOrUpdate(array $data): AbsenceStudent
     {
@@ -16,12 +17,6 @@ class AbsenceStudentRepository
             'absence_id' => $data['absence_id'],
             'student_id' => $data['student_id']
         ];
-
-        $existingRecord = $this->model->where($attributes)->first();
-
-        if ($existingRecord) {
-            throw new \Exception('Student has already been marked absent.');
-        }
 
         return $this->model->updateOrCreate($attributes, $data);
     }
